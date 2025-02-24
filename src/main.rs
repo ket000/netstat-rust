7b5326745bd55ec06629332d237d0a37138315c2
Ketan Kothari <ket000@gmail.com>
2025-02-23 20:12:10 -0500

    Updated Code to make it work with latest netstat2 0.11.1 and sysinfo 0.33.1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use std::cmp::Ordering;
-
 use netstat2::*;
-use sysinfo;
-use sysinfo::{ProcessExt, System, SystemExt};
+use sysinfo::System;
 
 struct ProcessInfo {
     pid: u32,
@@ -20,30 +17,23 @@ struct SocketInfo {
     family: AddressFamilyFlags,
 }
 
-// TODO: color output (ports/addr/state)
-// TODO: accept arguments - hide UDP by default
-
 fn main() {
-    let sys = System::new_all();
-
+    let mut sys = System::new_all();
+    // sys.refresh_processes(sysinfo::ProcessRefreshKind::everything(), sysinfo::RefreshKind::new());
+    //sys.refresh_processes(sysinfo::ProcessRefreshKind::everything(), true);
+      sys.refresh_processes(sysinfo::ProcessesToUpdate::All, true);
+    
     let mut sockets = get_sockets(&sys, AddressFamilyFlags::IPV4);
     let mut sockets6 = get_sockets(&sys, AddressFamilyFlags::IPV6);
     sockets.append(&mut sockets6);
-
-    // sort by port
-    sockets.sort_by(|a, b| {
-        if a.local_port < b.local_port {
-            Ordering::Less
-        } else {
-            Ordering::Greater
-        }
-    });
-
+    
+    sockets.sort_by(|a, b| a.local_port.cmp(&b.local_port));
+    
     println!("------------------------------");
     println!("TCP socket information");
     println!("------------------------------");
     print_tcp(&sockets);
-
+    
     println!();
     println!("------------------------------");
     println!("UDP socket information");
@@ -51,98 +41,68 @@ fn main() {
     print_udp(&sockets);
 }
 
-fn print_tcp(sockets: &Vec<SocketInfo>) {
+fn print_tcp(sockets: &[SocketInfo]) {
     for s in sockets {
         if s.protocol != ProtocolFlags::TCP {
             continue;
         }
 
-        let ip_ver = if s.family == AddressFamilyFlags::IPV4 {
-            "4"
-        } else {
-            "6"
-        };
+        let ip_ver = if s.family == AddressFamilyFlags::IPV4 { "4" } else { "6" };
+        let state_str = s.state.map_or("UNKNOWN".to_string(), |st| format!("{:?}", st));
+        let process_info = s.processes.get(0).map_or("Unknown Process".to_string(), |p| format!("{} ({})", p.name, p.pid));
 
         if s.state == Some(TcpState::Listen) {
             println!(
-                "TCP{:3}{:>30}:{:>5}    {:>6} {:<30}{:30}[{}]",
-                ip_ver,
-                format!("{}", s.local_addr),
-                s.local_port,
-                "",
-                "",
-                std::format!("{} ({})", s.processes[0].name, s.processes[0].pid),
-                s.state.unwrap()
+                "TCP{} {:>30}:{:<5}    {:<30} [{}]",
+                ip_ver, s.local_addr, s.local_port, process_info, state_str
             );
         } else {
             println!(
-                "TCP{:3}{:>30}:{:>5} -> {:>6}:{:<30}{:30}[{}]",
-                ip_ver,
-                format!("{}", s.local_addr),
-                s.local_port,
-                s.remote_port.unwrap(),
-                format!("{}", s.remote_addr.unwrap()),
-                std::format!("{} ({})", s.processes[0].name, s.processes[0].pid),
-                s.state.unwrap()
+                "TCP{} {:>30}:{:<5} -> {:>6}:{:<30} [{}]",
+                ip_ver, s.local_addr, s.local_port, s.remote_port.unwrap_or(0), s.remote_addr.map_or("-".to_string(), |a| a.to_string()), state_str
             );
         }
     }
 }
 
-fn print_udp(sockets: &Vec<SocketInfo>) {
+fn print_udp(sockets: &[SocketInfo]) {
     for s in sockets {
         if s.protocol != ProtocolFlags::UDP {
             continue;
         }
 
-        let ip_ver = if s.family == AddressFamilyFlags::IPV4 {
-            "4"
-        } else {
-            "6"
-        };
+        let ip_ver = if s.family == AddressFamilyFlags::IPV4 { "4" } else { "6" };
+        let process_info = s.processes.get(0).map_or("Unknown Process".to_string(), |p| format!("{} ({})", p.name, p.pid));
 
-        println!(
-            "UDP{:3}{:>30}:{:<8}{:30}",
-            ip_ver,
-            s.local_addr,
-            s.local_port,
-            std::format!("{} ({})", s.processes[0].name, s.processes[0].pid),
-        );
+        println!("UDP{} {:>30}:{:<8} {}", ip_ver, s.local_addr, s.local_port, process_info);
     }
 }
 
 fn get_sockets(sys: &System, addr: AddressFamilyFlags) -> Vec<SocketInfo> {
     let protos = ProtocolFlags::TCP | ProtocolFlags::UDP;
     let iterator = iterate_sockets_info(addr, protos).expect("Failed to get socket information!");
-
+    
     let mut sockets: Vec<SocketInfo> = Vec::new();
-
+    
     for info in iterator {
         let si = match info {
             Ok(si) => si,
-            Err(_err) => {
+            Err(_) => {
                 println!("Failed to get info for socket!");
                 continue;
             }
         };
-
-        // gather associated processes
-        let process_ids = si.associated_pids;
-        let mut processes: Vec<ProcessInfo> = Vec::new();
-        for pid in process_ids {
-            let name = match sys.get_process(pid as usize) {
-                Some(pinfo) => pinfo.name(),
-                None => "",
-            };
-            processes.push(ProcessInfo {
-                pid: pid,
-                name: name.to_string(),
-            });
-        }
-
+    
+        let processes: Vec<ProcessInfo> = si.associated_pids.iter().map(|&pid| {
+            let pid_obj = sysinfo::Pid::from_u32(pid);
+            let name = sys.process(pid_obj)
+                .map_or("".to_string(), |p| p.name().to_string_lossy().into_owned());
+            ProcessInfo { pid, name }
+        }).collect();
+    
         match si.protocol_socket_info {
             ProtocolSocketInfo::Tcp(tcp) => sockets.push(SocketInfo {
-                processes: processes,
+                processes,
                 local_port: tcp.local_port,
                 local_addr: tcp.local_addr,
                 remote_port: Some(tcp.remote_port),
@@ -152,7 +112,7 @@ fn get_sockets(sys: &System, addr: AddressFamilyFlags) -> Vec<SocketInfo> {
                 family: addr,
             }),
             ProtocolSocketInfo::Udp(udp) => sockets.push(SocketInfo {
-                processes: processes,
+                processes,
                 local_port: udp.local_port,
                 local_addr: udp.local_addr,
                 remote_port: None,
